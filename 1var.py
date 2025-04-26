51b83f37e4f7bd3c60d73b494c3b27994ed08939
apoorvavijay07 <152987556+apoorvavijay07@users.noreply.github.com>
2025-04-26 13:16:24 +0530

    1var.py

--- 1var.py
+++ 1var.py
@@ -15,3 +15,4 @@ l_n= " B"
 print(f_n+m_n+l_n)
 
 'Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue'
+' 1record : bcz variable cant start with digit'
